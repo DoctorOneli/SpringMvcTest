6c62c7888f40953a0f1acc4f478d2fa60fab07de
DoctorOneli <1006629349@qq.com>
2019-10-09 13:21:15 +0800

    添加数据校验功能

--- mvctest/src/main/java/org/freedom/mvctest/ControllerTest.java
+++ mvctest/src/main/java/org/freedom/mvctest/ControllerTest.java
@@ -1,6 +1,9 @@
 package org.freedom.mvctest;
 
+import javax.validation.Valid;
+
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,28 +11,45 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ControllerTest {
+
+	/**
+	 * 初始注册页面
+	 * 
+	 * @param model
+	 * @param user
+	 * @return
+	 */
 	@RequestMapping("/test")
-	public ModelAndView test(Model model)
-	{
-		ModelAndView modelAndView=new ModelAndView();
+	public ModelAndView test(Model model, User user) {
+		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("index");
+		modelAndView.addObject("user", user);
 		return modelAndView;
 	}
-	 
-	@RequestMapping("/register")
-	public ModelAndView Register(@ModelAttribute("form") User user) {  
-		// user:视图层传给控制层的表单对象；model：控制层返回给视图层的对象
-        // 在 model 中添加一个名为 "user" 的 user 对象
-		System.out.println(user.getUsername());
-		
-		ModelAndView modelAndView=new ModelAndView();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("success");
-		
-		return modelAndView;
-		
-}
 
-	
-	 
+	/**
+	 * 注册
+	 * 
+	 * @param user（校验）
+	 * @param bindingResult（校验结果）
+	 * @return
+	 */
+	@RequestMapping("/register")
+	public ModelAndView Register(@Valid User user, BindingResult bindingResult) {
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("user", user);
+
+		if (bindingResult.hasErrors()) {
+			modelAndView.setViewName("index");
+		}
+
+		else {
+			modelAndView.setViewName("success");
+		}
+
+		return modelAndView;
+
+	}
+
 }
--- mvctest/src/main/java/org/freedom/mvctest/User.java
+++ mvctest/src/main/java/org/freedom/mvctest/User.java
@@ -1,46 +1,67 @@
 package org.freedom.mvctest;
 
-import java.io.Serializable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
-public class User implements Serializable {
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
-    private static final long serialVersionUID = 1L;
-    private Integer id;
-    private String username;
-    private String password;
-    private Integer age;
+public class User  {
 
-    public Integer getId() {
-        return id;
-    }
+	private Integer id;
+	@NotBlank(message = "姓名不能为空")
+	@Pattern(regexp = "(^[a-zAZ_-]{2,13}$)|(^[\u2E80-\u9FFF]{2,6})", message = "姓名由2到5个汉字或2到13的英文组合")
+	private String username;
+	@NotBlank(message = "密码不能为空")
+	@Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,12}$", message = "密码要求含有数字和字母，且长度要在8-12位之间")
+	private String password;
+	@NotNull(message = "年龄不能为空")
+	@Range(max = 60, min = 18, message = "年龄范围为18到60")
+	private Integer age;
+	@NotBlank(message = "邮箱不能为空")
+	@Email(message = "请输入正确的邮箱格式")
+	private String email;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public Integer getAge() {
-        return age;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 }
-
